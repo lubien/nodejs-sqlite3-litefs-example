7d31c664b586fea26624793c93aca6b37fc5f7c9
lubien <lubien1996@gmail.com>
2023-07-13 13:50:17 -0300

    The port thingy

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 8080
+const port = process.env.PORT || 3000
 
 const dbConfig = require('./knexfile')[process.env.NODE_ENV || 'development']
 const knex = require('knex')(dbConfig);
